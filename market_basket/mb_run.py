1bffcfb8a3a55676890f9ef8c44738d7f7638988
allisonCYWu <allison.wu@thermofisher.com>
2019-08-09 22:59:31 -0700

    core market basket algorithm
    input columns: basket_key, prod_id, coupon, coupon_key
    
    output columns: coupon_X, coupon_key_X, coupon_Y, coupon_key_Y, basket_count_XY, basket_count_X, basket_count_Y, confidence, support, lift
    
    built-in test:
    - check validity of confidence: throw OutputOutOfBoundError if there's any record that has confidence >1 or <0.

--- market_basket/mb_run.py
+++ market_basket/mb_run.py
@@ -1,5 +1,17 @@
 from utility_functions.benchmark import timer
-from pyspark.sql.functions import col, lit, countDistinct
+from pyspark.sql.functions import col, lit, countDistinct, rand
+from utility_functions.custom_errors import *
+from utility_functions.databricks_uf import rdd_to_df
+import numpy
+import logging
+from connect2Databricks.spark_init import spark_init
+
+if 'spark' not in locals():
+    print('Environment: Databricks-Connect')
+    spark, sqlContext, setting = spark_init()
+
+module_logger = logging.getLogger('CVM.mb_run')
+
 
 @timer
 def market_basket_proc(data):
@@ -8,41 +20,87 @@ def market_basket_proc(data):
 
 @timer
 def market_basket_sql(data):
+    module_logger.info('===== market_basket_sql : START ======')
     total_basket_count = data.agg(countDistinct('basket_key'))
     total_basket_count = int(total_basket_count.toPandas()['count(DISTINCT basket_key)'][0])
     df = data.select('coupon_key', 'basket_key').\
         groupBy('coupon_key').\
         agg(countDistinct('basket_key').alias('basket_count'))
 
-    df1 = data.alias('df1').\
+    basket_counts = df.select('basket_count').toPandas()
+    basket_threshold = numpy.float64(numpy.percentile(basket_counts, 20)).item()
+    df = df.filter(col('basket_count') > lit(basket_threshold))
+
+    df1 = data.join(df, ['coupon_key'], how='inner').alias('df1').\
+        withColumnRenamed('basket_count', 'basket_count_X').\
         withColumnRenamed('coupon_key', 'coupon_key_X').\
         withColumnRenamed('coupon', 'coupon_X')
 
-    df2 = data.alias('df2').\
+    df2 = data.join(df, ['coupon_key'], how='inner').alias('df2'). \
+        withColumnRenamed('basket_count', 'basket_count_Y'). \
         withColumnRenamed('coupon_key', 'coupon_key_Y').\
         withColumnRenamed('coupon', 'coupon_Y')
 
-    matrix = df1.join(df2, (df1.coupon_key_X < df2.coupon_key_Y) & (df1.basket_key == df2.basket_key), how = 'inner'). \
-        select('coupon_key_X', 'coupon_X', 'coupon_key_Y', 'coupon_Y', 'df1.basket_key').\
+    matrix = df1.join(df2, (df1.coupon_key_X != df2.coupon_key_Y) & (df1.basket_key == df2.basket_key),
+                      how = 'inner'). \
+        select('coupon_key_X', 'coupon_X', 'coupon_key_Y', 'coupon_Y',
+               'basket_count_X', 'basket_count_Y',
+               'df1.basket_key').\
         withColumnRenamed('df1.basket_key', 'basket_key').\
-        groupBy('coupon_key_X', 'coupon_X', 'coupon_key_Y', 'coupon_Y'). \
+        groupBy('coupon_key_X', 'coupon_X', 'coupon_key_Y', 'coupon_Y', 'basket_count_X', 'basket_count_Y'). \
         agg(countDistinct('basket_key').alias('basket_count_XY')). \
-        select('coupon_key_X', 'coupon_X', 'coupon_key_Y', 'coupon_Y', 'basket_count_XY')
-
-    matrix = matrix.join(df, matrix.coupon_key_X == df.coupon_key, how = 'left').drop(df.coupon_key).\
-        withColumnRenamed('basket_count', 'basket_count_X')
-
-    matrix = matrix.join(df, matrix.coupon_key_Y == df.coupon_key, how = 'left').drop(df.coupon_key). \
-        withColumnRenamed('basket_count', 'basket_count_Y')
+        select('coupon_key_X', 'coupon_X', 'coupon_key_Y', 'coupon_Y', 'basket_count_X', 'basket_count_Y',
+               'basket_count_XY')
 
     matrix = matrix.\
         withColumn('confidence', col('basket_count_XY')/col('basket_count_X'))
 
     matrix = matrix.\
-        withColumn('lift', col('confidence')/col('basket_count_Y'))
+        withColumn('lift', col('confidence')/col('basket_count_Y')*lit(total_basket_count))
 
     matrix = matrix.\
         withColumn('support', col('basket_count_XY')/lit(total_basket_count))
 
+    rec_count = matrix.count()
+
+    conf_check_1 = matrix.filter(col('confidence') > 1.0).count()
+    conf_check_2 = matrix.filter(col('confidence') < 0).count()
+
+    if conf_check_1 > 0:
+        raise OutputOutOfBoundError(
+            f'Confidence should never be larger than 1 but there are {conf_check_1} records >0.'
+        )
+
+    if conf_check_2 > 0:
+        raise OutputOutOfBoundError(
+            f'Confidence should never be negative but there are {conf_check_2} records with negative confidence.'
+        )
+
+    if 'local' in setting:
+        try:
+            matrix = matrix.collect()
+            matrix = rdd_to_df(matrix)
+        except:
+            print('Cannot collect matrix.')
+
+    else:
+        matrix = matrix.cache()
+
+    module_logger.info(f'===== market_basket_sql : total_basket_count = {total_basket_count} ======')
+    module_logger.info(f'===== market_basket_sql : total_rec_count = {rec_count} ======')
+    module_logger.info('===== market_basket_sql : END ======')
     return total_basket_count, df, matrix
+
+
+def market_basket_stats(data, matrix):
+
+    # Permute the basket columns
+    i = 1
+    while i <= 1000:
+        rand_basket = data.select('basket_key')
+
+        i += 1
+
+    return df, matrix
+
 
