900c8d0f9a3f1480d52df5aad78cbf1b5ddaef22
allisonCYWu <allison.wu@thermofisher.com>
2019-08-18 23:37:52 -0700

    bug fix

--- division_view_market_basket/cvm_pre_processing.py
+++ division_view_market_basket/cvm_pre_processing.py
@@ -148,7 +148,7 @@ class MarketBasketPullHistory:
         else:
             print('Coupons is not defined for pulling sales.')
 
-        coupon_sales = sales.groupby('coupon', 'coupon_key').agg({'sum': 'sales'}). \
+        coupon_sales = sales.groupby('coupon', 'coupon_key').agg({'sales': 'sum'}). \
             withColumnRenamed('sum(sales)', 'coupon_sales'). \
             filter(col('coupon_sales') > 0)
 
